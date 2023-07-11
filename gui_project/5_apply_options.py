8728cabc19ad89b0ffd3c130432c653afe774df5
Junhyeok-Lim <122002292+Junhyeok-Lim@users.noreply.github.com>
2023-07-11 15:33:22 +0900

    Handling Exceptions

--- gui_project/5_apply_options.py
+++ gui_project/5_apply_options.py
@@ -37,98 +37,111 @@ def browse_dest_path():
 # 이미지 통합
 def merge_image():
 
-    # 가로 넓이
-    img_width = cmb_width.get()
-    if img_width == "원본유지" :
-        img_width = -1 # -1 일 때는 원본 기준으로
-    
-    else :
-        img_width = int(img_width)
+    try:
+        # 파일 목록 확인
+        if list_file.size() == 0:
+            msgbox.showwarning("경고", "이미지 파일을 추가하세요") # 파일이 없을 때 시작을 누르면 경고 메시지 출력
+            return
 
-    # 간격
-    img_space = cmb_space.get()
-    if img_space == "좁게":
-        img_space = 30
-    
-    elif img_space == "보통":
-        img_space = 60
+        # 저장 경로 확인
+        if len(txt_dest_path.get()) == 0:
+            msgbox.showwarning("경고", "저장 경로를 선택하세요") # 저장 경로 선택 안하고 시작을 누르면 경고 메시지 출력
+            return
+        
+        # 가로 넓이
+        img_width = cmb_width.get()
+        if img_width == "원본유지" :
+            img_width = -1 # -1 일 때는 원본 기준으로
+        
+        else :
+            img_width = int(img_width)
 
-    elif img_space == "넓게":
-        img_space = 90
+        # 간격
+        img_space = cmb_space.get()
+        if img_space == "좁게":
+            img_space = 30
+        
+        elif img_space == "보통":
+            img_space = 60
 
-    else :
-        img_space = 0
+        elif img_space == "넓게":
+            img_space = 90
 
-
-
-    # 포맷
-    img_format = cmb_format.get().lower() # PNG, JPG, 값을 받아와서 소문자로 변경
-
-
-    ###############################################################################
-
-    images = [Image.open(x) for x in list_file.get(0,END)]
-
-    # 이미지 사이즈 리스트에 넣어서 하나씩 처리
-    image_sizes = [] # [(width1, height1), (width2, height2), ...]
-    if img_width > -1:
-        # width 값 변경
-        image_sizes = [(int(img_width), int(img_width * x.size[1] / x.size[0])) for x in images]
-
-    else :
-        # 원본 사이즈 사용
-        image_sizes = [(x.size[0], x.size[1]) for x in images]
-    
-    # 계산식
-    # 100 * 60 이미지가 있음. -> width 를 80 으로 줄이면 height는?
-    # 원본 width : 원본 height = 변경 width : 변경 height
-    #     100    :     60     =        80    :     ?
-    #      x     :      y     =        x'    :     y'
-    #    xy' = x'y
-    #     y'  = x'y / x
-    #   100 : 60 = 80 : 48
-
-    # 우리 코드에 대입하려면?
-    # x = width = size[0]
-    # y = height = size[1]
-    # x' = img_width # 이 값으로 변경 해야 함
-    # y' = x'y / x = img_width * size[1] / size[0]
+        else :
+            img_space = 0
 
 
-    widths, heights = zip(*(image_sizes))
-
-    max_width, total_height = max(widths), sum(heights)
-    
-    # 스케치북 준비
-    result_img = Image.new("RGB", (max_width, total_height), (255, 255, 255)) # 흰 배경
-    y_offset = 0 # y 위치 정보
-    
-    for idx, img in enumerate(images):
-        result_img.paste(img, (0, y_offset))
-        y_offset += img.size[1]
-
-        progress = (idx + 1) / len(images) * 100 # 실제 퍼센트 정보를 계산
-        p_var.set(progress)
-        progress_bar.update()
-
-    dest_path = os.path.join(txt_dest_path.get(), "nado_photo.jpg")
-    result_img.save(dest_path)
-    msgbox.showinfo("알림", "작업이 완료되었습니다.")
+        # 포맷
+        img_format = cmb_format.get().lower() # PNG, JPG, 값을 받아와서 소문자로 변경
 
 
-# 시작
+        ###############################################################################
 
-def start():
-    
-    # 파일 목록 확인
-    if list_file.size() == 0:
-        msgbox.showwarning("경고", "이미지 파일을 추가하세요") # 파일이 없을 때 시작을 누르면 경고 메시지 출력
-        return
+        images = [Image.open(x) for x in list_file.get(0,END)]
 
-    # 저장 경로 확인
-    if len(txt_dest_path.get()) == 0:
-        msgbox.showwarning("경고", "저장 경로를 선택하세요") # 저장 경로 선택 안하고 시작을 누르면 경고 메시지 출력
-        return
+        # 이미지 사이즈 리스트에 넣어서 하나씩 처리
+        image_sizes = [] # [(width1, height1), (width2, height2), ...]
+        if img_width > -1:
+            # width 값 변경
+            image_sizes = [(int(img_width), int(img_width * x.size[1] / x.size[0])) for x in images]
+
+        else :
+            # 원본 사이즈 사용
+            image_sizes = [(x.size[0], x.size[1]) for x in images]
+        
+        # 계산식
+        # 100 * 60 이미지가 있음. -> width 를 80 으로 줄이면 height는?
+        # 원본 width : 원본 height = 변경 width : 변경 height
+        #     100    :     60     =        80    :     ?
+        #      x     :      y     =        x'    :     y'
+        #    xy' = x'y
+        #     y'  = x'y / x
+        #   100 : 60 = 80 : 48
+
+        # 우리 코드에 대입하려면?
+        # x = width = size[0]
+        # y = height = size[1]
+        # x' = img_width # 이 값으로 변경 해야 함
+        # y' = x'y / x = img_width * size[1] / size[0]
+
+
+        widths, heights = zip(*(image_sizes))
+
+        max_width, total_height = max(widths), sum(heights)
+        
+        # 스케치북 준비
+        if img_space > 0: # 이미지 간격 옵션 적용
+            total_height += ((img_space * len(images) -1))
+
+
+        result_img = Image.new("RGB", (max_width, total_height), (255, 255, 255)) # 흰 배경
+        y_offset = 0 # y 위치 정보
+        
+        for idx, img in enumerate(images):
+            # width 가 원본유지가 아닐 때에는 이미지 크기 조정
+            if img_width > -1 :
+                img = img.resize(image_sizes[idx])
+
+
+            result_img.paste(img, (0, y_offset))
+            y_offset += (img.size[1] + img_space) # height 값 + 사용자가 지정한 간격
+
+            progress = (idx + 1) / len(images) * 100 # 실제 퍼센트 정보를 계산
+            p_var.set(progress)
+            progress_bar.update()
+
+
+        # 포맷 옵션 처리
+        file_name = "nado photo." + img_format
+
+        dest_path = os.path.join(txt_dest_path.get(), file_name)
+        result_img.save(dest_path)
+        msgbox.showinfo("알림", "작업이 완료되었습니다.")
+
+    except Exception as err:
+        msgbox.showerror("에러", err)
+
+
     
 
 btn_add_file = Button(file_frame, padx=5, pady=5, width=12, text="파일추가", command=add_file)
